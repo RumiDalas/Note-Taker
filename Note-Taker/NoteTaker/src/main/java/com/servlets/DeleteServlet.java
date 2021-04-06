9c26f00abff879e760ffd38ce61ed95524c47c30
Rumi <76618296+RumiDalas@users.noreply.github.com>
2021-04-06 14:35:06 +0600

    Note Taker Application

---
+++ Note-Taker/NoteTaker/src/main/java/com/servlets/DeleteServlet.java
@@ -0,0 +1,67 @@
+package com.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.entities.Note;
+import com.helper.FactoryProvider;
+
+/**
+ * Servlet implementation class DeleteServlet
+ */
+public class DeleteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DeleteServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		try {
+			
+			int noteId = Integer.parseInt(request.getParameter("note_id").trim());
+			Session s =FactoryProvider.getFactory().openSession();
+			Transaction tx = s.beginTransaction();
+			
+			//Getting Note object
+			Note note =(Note)s.get(Note.class, noteId);
+			s.delete(note);
+			tx.commit();
+			response.sendRedirect("all_notes.jsp");
+			
+			
+			s.close();
+			
+			
+		}catch(Exception e) {
+			
+		}
+		
+		
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
